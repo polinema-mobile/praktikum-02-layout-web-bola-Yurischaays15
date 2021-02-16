e20c340aed248c0271fc167d88ce064f300c5b03
= <=>
2021-02-17 01:21:48 +0700

    Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
-import 'package:flutter/material.dart';
+import'package:flutter/cupertino.dart'; 
+import'package:flutter/material.dart'; 
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // home : Text('This is Text Widget',),
-      home:Image(image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Helianthus_annuus_exposed_2004-05-22.jpg/250px-Helianthus_annuus_exposed_2004-05-22.jpg'),),
-    );
-  }
+      home: Container(         
+        margin: EdgeInsets.only(top: 30),         
+        color: Colors.white,        
+        child: Column(           
+          children: <Widget>[             
+            AppBar(title: Text('Contoh Cupertino')),             
+            CupertinoButton(               
+              child: Text("Contoh button"),               
+              onPressed: () {},             
+              ),             
+              CupertinoActivityIndicator(),],         
+    ),       
+    ),     
+    );   
+    } 
 }
