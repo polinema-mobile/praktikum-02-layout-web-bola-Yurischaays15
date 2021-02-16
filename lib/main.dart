52d1bdf9c7ac987c1c0a4ce0635a5e3477c1c40c
= <=>
2021-02-17 01:10:30 +0700

    Image Widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home : Text('This is Text Widget',),
+      // home : Text('This is Text Widget',),
+      home:Image(image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Helianthus_annuus_exposed_2004-05-22.jpg/250px-Helianthus_annuus_exposed_2004-05-22.jpg'),),
     );
   }
 }
