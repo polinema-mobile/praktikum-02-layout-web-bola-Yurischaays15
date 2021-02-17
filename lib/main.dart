3fdc05f62434f9a0371c14415c180ecb7c45c35b
= <=>
2021-02-18 04:47:24 +0700

    Property Color

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
-//property alignment
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Aligment")),
-        body: Container(
-          alignment: Alignment.bottomCenter,
-          child: Text(
-            'Semangat Belajar Ya!',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ))),
+          appBar: AppBar(title: Text("Contoh Color")),
+          body: Container(
+              margin: EdgeInsets.all(50),
+              height: 200,
+              width: 200,
+              alignment: Alignment.center,
+              color: Colors.amber[900],
+              child: Text(
+                'Semangat Belajar Ya!',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              )
+              )
+              ),
     );
   }
 }
