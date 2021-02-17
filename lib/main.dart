7a9933ccc4248381f2b00ba35a41c5c5b2c82e7f
= <=>
2021-02-18 06:27:52 +0700

    Menambah Widget Berita Terbaru

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,28 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.red,
-          title: Text("MyApp")
-        )
-      )
+          title: Text("MyApp")),
+          body: GridView.count(
+            crossAxisCount:2,
+            children: <Widget> [
+            Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Text("BERITA TERBARU", style: TextStyle(fontSize:15, color: Colors.black)
+            ),
+            ),
+            Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Text("PERTANDINGAN HARI INI", style: TextStyle(fontSize:15, color: Colors.black)
+            ),
+            ),
+            SizedBox(
+                width: 5,
+            ),
+            ]
+        ),
+      ),
     );
   }
 }
