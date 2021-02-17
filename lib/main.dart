14973ee651fbb3ca718c86dc7463b4f8f5ef9845
= <=>
2021-02-17 14:46:53 +0700

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,32 @@
-import'package:flutter/cupertino.dart'; 
 import'package:flutter/material.dart'; 
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home:Scaffold(
-        floatingActionButton:FloatingActionButton(
-          onPressed: () {
-
-          },
-          child:Icon(Icons.thumb_up),
-          backgroundColor:Colors.pink,
+        appBar: AppBar (
+          title: Text('Sample Code'),
         ),
+        body: Center(
+          child: Text('You have pressed the button $_count times.'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 50.0,
+          ),
+        ),
+        floatingActionButton:FloatingActionButton(
+          onPressed: () => 0, 
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
