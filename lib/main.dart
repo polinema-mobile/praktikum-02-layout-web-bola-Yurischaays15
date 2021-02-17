bd6610843617d83e032624907be365e9377d8bee
= <=>
2021-02-18 05:13:13 +0700

    Membuat Judul

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh Margin")),
-        body: Container(
-            margin: EdgeInsets.all(50),
-            height: 200,
-            width: 200,
-            alignment: Alignment.topLeft,
-            color: Colors.blueGrey,
-            child: Text(
-              'Semangat Belajar Ya!',
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ))),
-        );
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text("MyApp")
+        )
+      )
+    );
   }
 }
