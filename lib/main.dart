0b44da1fb7432ce4a49c0583ce0f39a3ad335534
= <=>
2021-02-17 16:33:48 +0700

    Input dan Selection Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,25 @@
-import'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar (
-          title: Text('Sample Code'),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Contoh TextField")
         ),
-        body: Center(
-          child: Text('You have pressed the button $_count times.'),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Container(
-            height: 50.0,
+        body: TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
           ),
-        ),
-        floatingActionButton:FloatingActionButton(
-          onPressed: () => 0, 
-          tooltip: 'Increment Counter',
-          child: Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        )
       ),
     );
   }
