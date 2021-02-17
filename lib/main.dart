e82fdc3fb0914df21c913bd92880357aaddcdc06
= <=>
2021-02-17 16:45:33 +0700

    Property Alignmet

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,21 @@ void main() {
   runApp(MyApp());
 }
 
+//property alignment
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Contoh TextField")
-        ),
-        body: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
-          ),
-        )
-      ),
+        appBar: AppBar(title: Text("Contoh Aligment")),
+        body: Container(
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            'Semangat Belajar Ya!',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ))),
     );
   }
 }
